82e8003e14d33a5f9c251e29d2fb18bed3ccd463
wulfraem <wulfraem@users.noreply.github.com>
2020-07-08 14:18:44 +0200

    Update src/crypto/crypto_verifier.rs
    Co-authored-by: Sebastian Dechant <sebastian.dechant@evan.team>

--- src/crypto/crypto_verifier.rs
+++ src/crypto/crypto_verifier.rs
@@ -105,7 +105,7 @@ pub mod verifier {
                 }
 
                 sub_proof_request_builder = CryptoVerifier::new_sub_proof_request_builder()
-                    .map_err(|e| format!("could not create proof request builder: {}", &e))?;
+                    .map_err(|e| format!("could not create sub proof request builder: {}", &e))?;
                 for property in &sub_proof_request.revealed_attributes {
                     sub_proof_request_builder
                         .add_revealed_attr(&property)
