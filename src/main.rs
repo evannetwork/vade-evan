a28e25d609735b317ed620bdd71938eaa08bda58
wulfraem <wulfraem@users.noreply.github.com>
2020-08-03 08:41:09 +0200

    add cli interface
    - [CORE-1615]

--- src/main.rs
+++ src/main.rs
@@ -1,177 +1,217 @@
-use futures::executor::block_on;
-use tokio::net::TcpListener;
-use tokio::prelude::*;
+extern crate clap;
+use clap::{App, Arg, ArgMatches};
+use ursa::cl::prover::Prover;
 use vade::Vade;
 use vade_evan::{
     resolver::{ResolverConfig, SubstrateDidResolverEvan},
-    signing::{LocalSigner, Signer},
+    signing::{LocalSigner, RemoteSigner, Signer},
     VadeEvan,
 };
 
-async fn hello_world() {
-    println!("async hello, world!");
-}
-
-async fn get_and_log() {
-    let mut vade = get_vade().unwrap();
-
-    let results = vade
-        .did_resolve("did:evan:testcore:0x0d87204c3957d73b68ae28d0af961d3c72403906")
-        .await
-        .unwrap();
-
-    let err_msg = "could not get DID document";
-
-    let document_string = results[0]
-        .as_ref()
-        .ok_or_else(|| err_msg.to_string())
-        .unwrap()
-        .to_string();
-
-    println!("{}", &document_string);
-}
-
-async fn credo() {
-    let mut vade = get_vade().unwrap();
-
-    let options = r###"{
-        "privateKey": "dfcdcb6d5d09411ae9cbe1b0fd9751ba8803dd4b276d5bf9488ae4ede2669106",
-        "identity": "did:evan:testcore:0x0d87204C3957D73b68AE28d0AF961d3c72403906"
-    }"###;
-
-    let payload = r###"{
-        "schemaDid": "did:evan:zkp:0xccc8075b38209a6678dd8d110630458fe69d8b271f698a9c6b008ed3b1341902",
-        "issuerDid": "did:evan:testcore:0x0d87204C3957D73b68AE28d0AF961d3c72403906",
-        "issuerPublicKeyDid": "did:evan:testcore:0x0d87204C3957D73b68AE28d0AF961d3c72403906#key-1",
-        "issuerProvingKey": "dfcdcb6d5d09411ae9cbe1b0fd9751ba8803dd4b276d5bf9488ae4ede2669106"
-    }"###;
-
-    let results = vade
-        .vc_zkp_create_credential_definition("did:evan", &options, &payload)
-        .await
-        .unwrap();
-
-    let err_msg = "could not create credo";
-
-    let document_string = results[0]
-        .as_ref()
-        .ok_or_else(|| err_msg.to_string())
-        .unwrap()
-        .to_string();
-
-    println!("{}", &document_string);
-}
-
 #[tokio::main]
-async fn main() {
-    // let future = get_and_log(); // Nothing is printed
-    // block_on(future); // `future` is run and "hello, world!" is printed
-    // get_and_log().await;
-    credo().await;
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let matches = get_args();
+
+    let mut vade = get_vade(
+        get_arg(&matches, "target", Some("13.69.59.185")),
+        get_arg(&matches, "signer", Some("local")),
+    )
+    .unwrap();
+
+    let command = get_arg(&matches, "command", Some(""));
+    let mut did = "";
+    let mut method = "";
+
+    if command.starts_with("did_") {
+        did = get_arg(&matches, "did", None);
+    } else if command.starts_with("vc_zkp_") {
+        method = get_arg(&matches, "method", None);
+    }
+
+    let options = get_arg(&matches, "options", Some(""));
+    let payload = get_arg(&matches, "payload", Some(""));
+
+    let results = match command {
+        "create_master_secret" => vec![Some(
+            serde_json::to_string(&Prover::new_master_secret().unwrap()).unwrap(),
+        )],
+        "did_create" => vade.did_create(&did, &options, &payload).await.unwrap(),
+        "did_resolve" => vade.did_resolve(&did).await.unwrap(),
+        "did_update" => vade.did_update(&did, &options, &payload).await?,
+        "vc_zkp_create_credential_definition" => vade
+            .vc_zkp_create_credential_definition(&method, &options, &payload)
+            .await
+            .unwrap(),
+        "vc_zkp_create_credential_schema" => vade
+            .vc_zkp_create_credential_schema(&method, &options, &payload)
+            .await
+            .unwrap(),
+        "vc_zkp_create_revocation_registry_definition" => vade
+            .vc_zkp_create_revocation_registry_definition(&method, &options, &payload)
+            .await
+            .unwrap(),
+        "vc_zkp_issue_credential" => vade
+            .vc_zkp_issue_credential(&method, &options, &payload)
+            .await
+            .unwrap(),
+        "vc_zkp_create_credential_offer" => vade
+            .vc_zkp_create_credential_offer(&method, &options, &payload)
+            .await
+            .unwrap(),
+        "vc_zkp_present_proof" => vade
+            .vc_zkp_present_proof(&method, &options, &payload)
+            .await
+            .unwrap(),
+        "vc_zkp_create_credential_proposal" => vade
+            .vc_zkp_create_credential_proposal(&method, &options, &payload)
+            .await
+            .unwrap(),
+        "vc_zkp_request_credential" => vade
+            .vc_zkp_request_credential(&method, &options, &payload)
+            .await
+            .unwrap(),
+        "vc_zkp_request_proof" => vade
+            .vc_zkp_request_proof(&method, &options, &payload)
+            .await
+            .unwrap(),
+        "vc_zkp_revoke_credential" => vade
+            .vc_zkp_revoke_credential(&method, &options, &payload)
+            .await
+            .unwrap(),
+        "vc_zkp_verify_proof" => vade
+            .vc_zkp_verify_proof(&method, &options, &payload)
+            .await
+            .unwrap(),
+        _ => panic!("unsupported command: {}", &command),
+    };
+    if results.is_empty() {
+        panic!("no results");
+    }
+
+    let empty_result = String::new();
+    let result_string = results[0]
+        .as_ref()
+        .or(Some(&empty_result))
+        .unwrap()
+        .to_string();
+
+    println!("{}", &result_string);
+
+    Ok(())
 }
 
-// fn get_config_values(
-//     config: Option<&JsValue>,
-//     keys: Vec<String>,
-// ) -> Result<Vec<String>, Box<dyn std::error::Error>> {
-//     let mut vec = Vec::new();
-//     let mut config_undefined = true;
+fn get_arg<'a>(matches: &'a ArgMatches, arg_name: &'a str, fallback: Option<&'a str>) -> &'a str {
+    match matches.value_of(arg_name) {
+        Some(value) => value,
+        None => match fallback {
+            Some(value) => value,
+            None => {
+                panic!("no value for {} given", arg_name);
+            }
+        },
+    }
+}
 
-//     let config_hash_map: HashMap<String, String>;
-//     match config {
-//         Some(value) => {
-//             if !value.is_undefined() {
-//                 config_hash_map = value.into_serde()?;
-//                 config_undefined = false;
-//             } else {
-//                 config_hash_map = HashMap::<String, String>::new();
-//             }
-//         }
-//         None => {
-//             config_hash_map = HashMap::<String, String>::new();
-//         }
-//     };
+fn get_args() -> ArgMatches<'static> {
+    App::new("vade_evan_bin")
+        .version("0.0.6")
+        .author("evan GmbH")
+        .about("allows you to use to work with DIDs and zero knowledge proof VCs on Trust and Trace")
+        .arg(
+            Arg::with_name("command")
+                .long("command")
+                .short("c")
+                .required(true)
+                .value_name("command")
+                .help("vade command, e.g. 'vc_zkp_issue_credential'")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("did")
+                .long("did")
+                .short("d")
+                .value_name("did")
+                .help("a DID to work on, e.g. 'did:evan:testcore:0x0d87204c3957d73b68ae28d0af961d3c72403906'")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("method")
+                .long("method")
+                .short("m")
+                .value_name("method")
+                .help("method to work on, e.g. 'did:evan'")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("options")
+                .long("options")
+                .short("o")
+                .value_name("options")
+                .help("options to send to vade call, serialized JSON, e.g. '{ \"identity\": \"...\", \"privateKey\": \"...\" }'")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("payload")
+                .long("payload")
+                .short("p")
+                .value_name("payload")
+                .help("options to send to vade call, serialized JSON, e.g. '{ \"foo\": \"bar\" }'")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("target")
+                .long("target")
+                .short("t")
+                .value_name("target")
+                .help("substrate to use for DID handling, e.g. '127.0.0.1'")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("signer")
+                .long("signer")
+                .short("s")
+                .value_name("signer")
+                .help("signer to use to sign messages with, e.g. 'local' or 'remote|http://somewhere'")
+                .takes_value(true),
+        )
+        .get_matches()
+}
 
-//     for key in keys {
-//         if config_undefined || !config_hash_map.contains_key(&key) {
-//             vec.push(get_config_default(&key)?);
-//         } else {
-//             vec.push(
-//                 config_hash_map
-//                     .get(&key)
-//                     .ok_or_else(|| format!("could not get key '{}' from config", &key))?
-//                     .to_string(),
-//             );
-//         }
-//     }
+fn get_signer(signer: &str) -> Box<dyn Signer> {
+    if signer.starts_with("remote") {
+        Box::new(RemoteSigner::new(
+            signer.trim_start_matches("remote|").to_string(),
+        ))
+    } else if signer.starts_with("local") {
+        Box::new(LocalSigner::new())
+    } else {
+        panic!("invalid signer config")
+    }
+}
 
-//     Ok(vec)
-// }
-
-// fn get_config_default(key: &str) -> Result<String, Box<dyn std::error::Error>> {
-//     Ok(match key {
-//         "signer" => "remote|https://tntkeyservices-e0ae.azurewebsites.net/api/key/sign",
-//         // "signer" => "local",
-//         "target" => "13.69.59.185",
-//         _ => return Err(Box::from(format!("invalid invalid config key '{}'", key))),
-//     }
-//     .to_string())
-// }
-
-// #[cfg(feature = "vc-zkp")]
-// fn get_options(private_key: String, identity: String) -> String {
-//     format!(
-//         r###"{{
-//             "privateKey": "{}",
-//             "identity": "{}"
-//         }}"###,
-//         private_key, identity,
-//     )
-// }
-
-// fn get_signer(signer_config: String) -> Result<Box<dyn Signer>, Box<dyn std::error::Error>> {
-//     if signer_config == "local" {
-//         Ok(Box::new(LocalSigner::new()))
-//     } else if signer_config.starts_with("remote|") {
-//         Ok(Box::new(RemoteSigner::new(
-//             signer_config
-//                 .strip_prefix("remote|")
-//                 .ok_or("invalid signer_config")?
-//                 .to_string(),
-//         )))
-//     } else {
-//         Err(Box::from(format!(
-//             "invalid signer config {}",
-//             &signer_config
-//         )))
-//     }
-// }
-
-fn get_vade() -> Result<Vade, Box<dyn std::error::Error>> {
+fn get_vade(target: &str, signer: &str) -> Result<Vade, Box<dyn std::error::Error>> {
     let mut vade = Vade::new();
 
-    let signer: Box<dyn Signer> = Box::new(LocalSigner::new());
+    let signer_box: Box<dyn Signer> = get_signer(signer);
+
     vade.register_plugin(Box::from(SubstrateDidResolverEvan::new(ResolverConfig {
-        signer,
-        target: "13.69.59.185".to_string(),
+        signer: signer_box,
+        target: target.to_string(),
     })));
 
-    vade.register_plugin(Box::from(get_vade_evan()?));
+    vade.register_plugin(Box::from(get_vade_evan(target, signer)?));
 
     Ok(vade)
 }
 
-fn get_vade_evan() -> Result<VadeEvan, Box<dyn std::error::Error>> {
+fn get_vade_evan(target: &str, signer: &str) -> Result<VadeEvan, Box<dyn std::error::Error>> {
     let mut internal_vade = Vade::new();
-    let signer: Box<dyn Signer> = Box::new(LocalSigner::new());
+    let signer_box: Box<dyn Signer> = get_signer(signer);
     internal_vade.register_plugin(Box::from(SubstrateDidResolverEvan::new(ResolverConfig {
-        signer,
-        // target: "127.0.0.1".to_string(),
-        target: "13.69.59.185".to_string(),
+        signer: signer_box,
+        target: target.to_string(),
     })));
-    let signer: Box<dyn Signer> = Box::new(LocalSigner::new());
+    let signer: Box<dyn Signer> = get_signer(signer);
 
     Ok(VadeEvan::new(internal_vade, signer))
 }
-
-// fn main() {}
