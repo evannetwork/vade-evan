a91fd53e30fa8329144a1a24580654d5241a8964
wulfraem <wulfraem@users.noreply.github.com>
2020-08-11 17:25:27 +0200

    refactor test data - move data into `test_data` module
    - [CORE-1630]

--- tests/crypto_utils.rs
+++ tests/crypto_utils.rs
@@ -20,38 +20,18 @@ mod test_data;
 
 use serde::{Deserialize, Serialize};
 use std::{collections::HashMap, env, error::Error};
-use test_data::{SIGNER_LOCAL_ADDRESS, SIGNER_LOCAL_PRIVATE_KEY};
+use test_data::{
+    EXAMPLE_CREDENTIAL_SCHEMA,
+    SIGNER_LOCAL_ADDRESS,
+    SIGNER_LOCAL_DID_DOCUMENT_JWS,
+    SIGNER_LOCAL_IDENTITY,
+    SIGNER_LOCAL_PRIVATE_KEY,
+};
 use vade_evan::{
     application::datatypes::{CredentialSchema, SchemaProperty},
     crypto::crypto_utils::{create_assertion_proof, recover_address_and_data, JwsData},
     signing::{LocalSigner, RemoteSigner, Signer},
 };
-
-const DOCUMENT_TO_SIGN: &str = r###"
-{
-  "id": "did:evan:zkp:0x123451234512345123451234512345",
-  "type": "EvanVCSchema",
-  "name": "test_schema",
-  "author": "did:evan:testcore:0x0F737D1478eA29df0856169F25cA9129035d6FD1",
-  "createdAt": "2020-05-19T12:54:55.000Z",
-  "description": "Test description",
-  "properties": {
-    "test_property_string": {
-      "type": "string"
-    }
-  },
-  "required": [
-    "test_property_string"
-  ],
-  "additionalProperties": false
-}
-"###;
-
-const ISSUER: &str = "did:evan:testcore:0x0f737d1478ea29df0856169f25ca9129035d6fd1";
-const ISSUER_ETHEREUM_ADDRESS: &str = "0x775018c020ae1b3fd4e8a707f8ecfeafc9055e9d";
-const VERIFICATION_METHOD: &str =
-    "did:evan:testcore:0x0f737d1478ea29df0856169f25ca9129035d6fd1#key-1";
-const EXPECTED_SIGNATURE: &str = "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NkstUiJ9.eyJpYXQiOjE1ODk4MDIxMjYsImRpZERvY3VtZW50Ijp7IkBjb250ZXh0IjoiaHR0cHM6Ly93M2lkLm9yZy9kaWQvdjEiLCJpZCI6ImRpZDpldmFuOnRlc3Rjb3JlOjB4MGY3MzdkMTQ3OGVhMjlkZjA4NTYxNjlmMjVjYTkxMjkwMzVkNmZkMSIsInB1YmxpY0tleSI6W3siaWQiOiJkaWQ6ZXZhbjp0ZXN0Y29yZToweDBmNzM3ZDE0NzhlYTI5ZGYwODU2MTY5ZjI1Y2E5MTI5MDM1ZDZmZDEja2V5LTEiLCJ0eXBlIjoiU2VjcDI1NmsxVmVyaWZpY2F0aW9uS2V5MjAxOCIsImNvbnRyb2xsZXIiOiJkaWQ6ZXZhbjp0ZXN0Y29yZToweDBmNzM3ZDE0NzhlYTI5ZGYwODU2MTY5ZjI1Y2E5MTI5MDM1ZDZmZDEiLCJldGhlcmV1bUFkZHJlc3MiOiIweDc3NTAxOGMwMjBhZTFiM2ZkNGU4YTcwN2Y4ZWNmZWFmYzkwNTVlOWQifV0sImF1dGhlbnRpY2F0aW9uIjpbImRpZDpldmFuOnRlc3Rjb3JlOjB4MGY3MzdkMTQ3OGVhMjlkZjA4NTYxNjlmMjVjYTkxMjkwMzVkNmZkMSNrZXktMSJdLCJjcmVhdGVkIjoiMjAyMC0wNS0xOFQxMTo0MjowNi43NjZaIiwidXBkYXRlZCI6IjIwMjAtMDUtMThUMTE6NDI6MDYuNzY2WiJ9LCJpc3MiOiJkaWQ6ZXZhbjp0ZXN0Y29yZToweDBmNzM3ZDE0NzhlYTI5ZGYwODU2MTY5ZjI1Y2E5MTI5MDM1ZDZmZDEifQ.MBBWlq_zH5cRzlpYcc4eoX_qbg2ICG3V-MZj-5TVPzhhIAE7dJdxREQPNtBya9Rk5sWc4bItJDvOyq4hwKX66wA";
 
 #[derive(Serialize, Deserialize)]
 struct JwsDoc {
@@ -66,18 +46,25 @@ struct JwsDoc {
 
 #[test]
 fn can_recover_address_and_data_from_signature() {
-    let (address, data) = recover_address_and_data(EXPECTED_SIGNATURE).unwrap();
-    assert_eq!(format!("0x{}", address), ISSUER_ETHEREUM_ADDRESS);
+    let (address, data) = recover_address_and_data(SIGNER_LOCAL_DID_DOCUMENT_JWS).unwrap();
+    assert_eq!(format!("0x{}", address), SIGNER_LOCAL_ADDRESS);
 
     // if we find these strings, we can assume the recovery is fine
+    println!("data: {}", &data);
     assert_eq!(
         true,
-        data.contains(r#""id":"did:evan:testcore:0x0f737d1478ea29df0856169f25ca9129035d6fd1""#)
+        data.contains(&format!(r#""id":"{}""#, &SIGNER_LOCAL_IDENTITY))
     );
-    assert_eq!(true, data.contains(r##""publicKey":[{"id":"did:evan:testcore:0x0f737d1478ea29df0856169f25ca9129035d6fd1#key-1""##));
     assert_eq!(
         true,
-        data.contains(r#"ethereumAddress":"0x775018c020ae1b3fd4e8a707f8ecfeafc9055e9d"#)
+        data.contains(&format!(
+            r##""publicKey":[{{"id":"{}#key-1""##,
+            &SIGNER_LOCAL_IDENTITY
+        ))
+    );
+    assert_eq!(
+        true,
+        data.contains(&format!(r#"ethereumAddress":"{}"#, &SIGNER_LOCAL_ADDRESS))
     );
 }
 
@@ -88,13 +75,13 @@ async fn can_create_assertion_proof() -> Result<(), Box<dyn Error>> {
     };
 
     // First deserialize it into a data type or else serde_json will serialize the document into raw unformatted text
-    let schema: CredentialSchema = serde_json::from_str(DOCUMENT_TO_SIGN).unwrap();
+    let schema: CredentialSchema = serde_json::from_str(EXAMPLE_CREDENTIAL_SCHEMA).unwrap();
     let doc_to_sign = serde_json::to_value(&schema).unwrap();
     let signer: Box<dyn Signer> = Box::new(LocalSigner::new());
     let proof = create_assertion_proof(
         &doc_to_sign,
-        VERIFICATION_METHOD,
-        ISSUER,
+        &format!("{}#key-1", &SIGNER_LOCAL_IDENTITY),
+        SIGNER_LOCAL_IDENTITY,
         &SIGNER_LOCAL_PRIVATE_KEY,
         &signer,
     )
@@ -103,13 +90,16 @@ async fn can_create_assertion_proof() -> Result<(), Box<dyn Error>> {
 
     assert_eq!(proof.proof_purpose, "assertionMethod".to_owned());
     assert_eq!(proof.r#type, "EcdsaPublicKeySecp256k1".to_owned());
-    assert_eq!(proof.verification_method, VERIFICATION_METHOD.to_owned());
+    assert_eq!(
+        proof.verification_method,
+        format!("{}#key-1", &SIGNER_LOCAL_IDENTITY)
+    );
 
     // Recover document from signature and check if it equals the original
     let (address, data) = recover_address_and_data(&proof.jws).unwrap();
     let jws: JwsData = serde_json::from_str(&data).unwrap();
     let doc: JwsDoc = serde_json::from_str(jws.doc.get()).unwrap();
-    let orig: JwsDoc = serde_json::from_str(DOCUMENT_TO_SIGN).unwrap();
+    let orig: JwsDoc = serde_json::from_str(EXAMPLE_CREDENTIAL_SCHEMA).unwrap();
     assert_eq!(
         serde_json::to_string(&doc).unwrap(),
         serde_json::to_string(&orig).unwrap()
@@ -119,7 +109,6 @@ async fn can_create_assertion_proof() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-// #[ignore]
 #[tokio::test]
 async fn can_sign_messages_remotely() -> Result<(), Box<dyn Error>> {
     let signer = RemoteSigner::new(
