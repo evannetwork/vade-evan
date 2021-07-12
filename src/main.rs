fce1079aa69070b8585b0c94c5bbbf2b2d61f3b5
Sebastian Wolfram <wulfraem@users.noreply.github.com>
2021-07-12 10:18:23 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- src/main.rs
+++ src/main.rs
@@ -48,10 +48,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let results = match matches.subcommand() {
         ("did", Some(sub_m)) => match sub_m.subcommand() {
             ("create", Some(sub_m)) => {
-                let did = get_argument_value(&sub_m, "method", None);
+                let method = get_argument_value(&sub_m, "method", None);
                 let options = get_argument_value(&sub_m, "options", None);
                 get_vade(&sub_m)?
-                    .did_create(&did, &options, &String::new())
+                    .did_create(&method, &options, &String::new())
                     .await?
             }
             ("resolve", Some(sub_m)) => {
