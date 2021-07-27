1b669be1754ba53d2aaa4dcc12407912035c5966
vineet pant <10172895+vineetpant@users.noreply.github.com>
2021-07-27 08:02:22 +0200

    added vade functions wrapper

--- src/c_lib.rs
+++ src/c_lib.rs
@@ -14,14 +14,13 @@
   limitations under the License.
 */
 
-use std::ffi::{CStr,CString};
-use std::{collections::HashMap, error::Error};
+use crate::vade_utils::{get_config_default, get_vade as get_vade_from_utils};
+use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
-use vade::{Vade, VadePluginResultValue};
-use crate::vade_utils::{get_vade as get_vade_from_utils, get_config_default};
 use std::slice;
-use futures::executor::block_on;
-
+use std::{collections::HashMap, error::Error};
+use tokio::runtime::Builder;
+use vade::Vade;
 
 macro_rules! handle_results {
     ($func_name:expr, $did_or_method:expr, $results:expr) => {
@@ -32,7 +31,9 @@ macro_rules! handle_results {
         ensure($results.len() > 0, || (&err_msg).to_string())?;
 
         let empty_result = &String::new();
-        return Ok(Some($results[0].as_ref().unwrap_or(empty_result).to_string()))
+        return Ok(Some(
+            $results[0].as_ref().unwrap_or(empty_result).to_string(),
+        ))
     };
 }
 
@@ -90,12 +91,14 @@ macro_rules! create_function {
                 .$func_name(&did_or_method, &function, &options, &payload)
                 .await
                 .map_err(jsify)?;
-                handle_results!(format!("{}: {}", stringify!($func_name), &function), did_or_method, results);
+            handle_results!(
+                format!("{}: {}", stringify!($func_name), &function),
+                did_or_method,
+                results
+            );
         }
     };
 }
-
-
 
 cfg_if::cfg_if! {
     if #[cfg(feature = "did")] {
@@ -151,8 +154,15 @@ fn jsify(err: Box<dyn Error>) -> String {
 
 #[allow(unused_variables)] // allow possibly unused variables due to feature mix
 pub fn get_vade(config: Option<&String>) -> Result<Vade, Box<dyn Error>> {
+    println!("get_vade");
     let config_values =
         get_config_values(config, vec!["signer".to_string(), "target".to_string()])?;
+
+    println!(
+        "config {}",
+        config_values.clone().into_iter().collect::<String>()
+    );
+
     let (signer_config, target) = match config_values.as_slice() {
         [signer_config, target, ..] => (signer_config, target),
         _ => {
@@ -160,9 +170,9 @@ pub fn get_vade(config: Option<&String>) -> Result<Vade, Box<dyn Error>> {
         }
     };
 
+    println!("(target {} signer_config {}", target, signer_config);
     return get_vade_from_utils(target, signer_config);
 }
-
 
 fn get_config_values(
     config: Option<&String>,
@@ -171,19 +181,17 @@ fn get_config_values(
     let mut vec = Vec::new();
     let mut config_undefined = true;
 
-    let config_hash_map: HashMap<String, String> ;
-    // let config_values = 
+    let config_hash_map: HashMap<String, String>;
+    // let config_values =
 
     match config {
         Some(value) => {
             if !value.is_empty() {
-               
                 config_hash_map = serde_json::from_str(&value)?;
                 config_undefined = false;
-            }else{
+            } else {
                 config_hash_map = HashMap::<String, String>::new();
-
-            } 
+            }
         }
         None => {
             config_hash_map = HashMap::<String, String>::new();
@@ -206,49 +214,166 @@ fn get_config_values(
     Ok(vec)
 }
 
-
-
 #[no_mangle]
-pub extern "C" fn execute_vade(func_name: *const c_char, arguments: *const *const c_char,  num_of_args: usize, config: *const *const c_char)-> *const c_char{
+pub extern "C" fn execute_vade(
+    func_name: *const c_char,
+    arguments: *const *const c_char,
+    num_of_args: usize,
+    options: *const c_char,
+    config: *const c_char,
+) -> *const c_char {
     let func = unsafe { CStr::from_ptr(func_name).to_string_lossy().into_owned() };
-    let args_array: &[*const c_char] = unsafe { slice::from_raw_parts(arguments, num_of_args as usize) };
-
+    let args_array: &[*const c_char] =
+        unsafe { slice::from_raw_parts(arguments, num_of_args as usize) };
     // convert each element to a Rust string
     let arguments_vec: Vec<_> = args_array
         .iter()
         .map(|&v| unsafe { CStr::from_ptr(v).to_string_lossy().into_owned() })
         .collect();
-    println!("function {}",func);
-    // println!()
-    let no_args  = String::from("");
- 
+
+    let options = unsafe { CStr::from_ptr(options).to_string_lossy().into_owned() };
+    let config = unsafe { CStr::from_ptr(config).to_string_lossy().into_owned() };
+
+    println!("function {}", func);
+    println!(
+        "args {}",
+        arguments_vec.clone().into_iter().collect::<String>()
+    );
+
+    let no_args = String::from("");
+
+    let runtime = Builder::new_current_thread()
+        .enable_io()
+        .build()
+        .expect("Failed to create runtime");
+
     let result = match func.as_str() {
-        "did_resolve" => block_on(did_resolve(arguments_vec.get(0).unwrap_or_else( || &no_args).to_owned(), "".to_string())),
-        _ => Err("No match found".to_string())
-        // "did_create" => did_create(did_or_method, options, payload, config),
-        // "did_update" => did_update(did_or_method, options, payload, config),
-        // "didcomm_receive" => didcomm_receive(options, payload, config),
-        // "didcomm_send" => didcomm_send(options, payload, config),
-        // "vc_zkp_create_credential_definition" => vc_zkp_create_credential_definition(did_or_method, options, payload, config),
-        // "vc_zkp_create_credential_offer" => vc_zkp_create_credential_offer(did_or_method, options, payload, config),
-        // "vc_zkp_create_credential_proposal" => vc_zkp_create_credential_proposal(did_or_method, options, payload, config),
-        // "vc_zkp_create_credential_schema" => vc_zkp_create_credential_schema(did_or_method, options, payload, config),
-        // "vc_zkp_create_revocation_registry_definition" => vc_zkp_create_revocation_registry_definition(did_or_method, options, payload, config),
-        // "vc_zkp_update_revocation_registry" => vc_zkp_update_revocation_registry(did_or_method, options, payload, config),
-        // "vc_zkp_issue_credential" => vc_zkp_issue_credential(did_or_method, options, payload, config),
-        // "vc_zkp_finish_credential" => vc_zkp_finish_credential(did_or_method, options, payload, config),
-        // "vc_zkp_present_proof" => vc_zkp_present_proof(did_or_method, options, payload, config),
-        // "vc_zkp_request_credential" => vc_zkp_request_credential(did_or_method, options, payload, config),
-        // "vc_zkp_request_proof" => vc_zkp_request_proof(did_or_method, options, payload, config),
-        // "vc_zkp_revoke_credential" => vc_zkp_revoke_credential(did_or_method, options, payload, config),
-        // "vc_zkp_verify_proof" => vc_zkp_verify_proof(did_or_method, options, payload, config),
+        "did_resolve" => runtime.block_on(did_resolve(
+            arguments_vec.get(0).unwrap_or_else(|| &no_args).to_owned(),
+            config,
+        )),
+        "did_create" => runtime.block_on(did_create(
+            arguments_vec.get(0).unwrap_or_else(|| &no_args).to_owned(),
+            options,
+            arguments_vec.get(1).unwrap_or_else(|| &no_args).to_owned(),
+            config,
+        )),
+        "did_update" => runtime.block_on(did_update(
+            arguments_vec.get(0).unwrap_or_else(|| &no_args).to_owned(),
+            options,
+            arguments_vec.get(1).unwrap_or_else(|| &no_args).to_owned(),
+            config,
+        )),
+        "didcomm_receive" => runtime.block_on(didcomm_receive(
+            options,
+            arguments_vec.get(0).unwrap_or_else(|| &no_args).to_owned(),
+            config,
+        )),
+        "didcomm_send" => runtime.block_on(didcomm_send(
+            options,
+            arguments_vec.get(0).unwrap_or_else(|| &no_args).to_owned(),
+            config,
+        )),
+        "vc_zkp_create_credential_definition" => {
+            runtime.block_on(vc_zkp_create_credential_definition(
+                arguments_vec.get(0).unwrap_or_else(|| &no_args).to_owned(),
+                options,
+                arguments_vec.get(1).unwrap_or_else(|| &no_args).to_owned(),
+                config,
+            ))
+        }
+        "vc_zkp_create_credential_offer" => runtime.block_on(vc_zkp_create_credential_offer(
+            arguments_vec.get(0).unwrap_or_else(|| &no_args).to_owned(),
+            options,
+            arguments_vec.get(1).unwrap_or_else(|| &no_args).to_owned(),
+            config,
+        )),
+        "vc_zkp_create_credential_proposal" => runtime.block_on(vc_zkp_create_credential_proposal(
+            arguments_vec.get(0).unwrap_or_else(|| &no_args).to_owned(),
+            options,
+            arguments_vec.get(1).unwrap_or_else(|| &no_args).to_owned(),
+            config,
+        )),
+        "vc_zkp_create_credential_schema" => runtime.block_on(vc_zkp_create_credential_schema(
+            arguments_vec.get(0).unwrap_or_else(|| &no_args).to_owned(),
+            options,
+            arguments_vec.get(1).unwrap_or_else(|| &no_args).to_owned(),
+            config,
+        )),
+        "vc_zkp_create_revocation_registry_definition" => {
+            runtime.block_on(vc_zkp_create_revocation_registry_definition(
+                arguments_vec.get(0).unwrap_or_else(|| &no_args).to_owned(),
+                options,
+                arguments_vec.get(1).unwrap_or_else(|| &no_args).to_owned(),
+                config,
+            ))
+        }
+        "vc_zkp_update_revocation_registry" => runtime.block_on(vc_zkp_update_revocation_registry(
+            arguments_vec.get(0).unwrap_or_else(|| &no_args).to_owned(),
+            options,
+            arguments_vec.get(1).unwrap_or_else(|| &no_args).to_owned(),
+            config,
+        )),
+        "vc_zkp_issue_credential" => runtime.block_on(vc_zkp_issue_credential(
+            arguments_vec.get(0).unwrap_or_else(|| &no_args).to_owned(),
+            options,
+            arguments_vec.get(1).unwrap_or_else(|| &no_args).to_owned(),
+            config,
+        )),
+        "vc_zkp_finish_credential" => runtime.block_on(vc_zkp_finish_credential(
+            arguments_vec.get(0).unwrap_or_else(|| &no_args).to_owned(),
+            options,
+            arguments_vec.get(1).unwrap_or_else(|| &no_args).to_owned(),
+            config,
+        )),
+        "vc_zkp_present_proof" => runtime.block_on(vc_zkp_present_proof(
+            arguments_vec.get(0).unwrap_or_else(|| &no_args).to_owned(),
+            options,
+            arguments_vec.get(1).unwrap_or_else(|| &no_args).to_owned(),
+            config,
+        )),
+        "vc_zkp_request_credential" => runtime.block_on(vc_zkp_request_credential(
+            arguments_vec.get(0).unwrap_or_else(|| &no_args).to_owned(),
+            options,
+            arguments_vec.get(1).unwrap_or_else(|| &no_args).to_owned(),
+            config,
+        )),
+        "vc_zkp_request_proof" => runtime.block_on(vc_zkp_request_proof(
+            arguments_vec.get(0).unwrap_or_else(|| &no_args).to_owned(),
+            options,
+            arguments_vec.get(1).unwrap_or_else(|| &no_args).to_owned(),
+            config,
+        )),
+        "vc_zkp_revoke_credential" => runtime.block_on(vc_zkp_revoke_credential(
+            arguments_vec.get(0).unwrap_or_else(|| &no_args).to_owned(),
+            options,
+            arguments_vec.get(1).unwrap_or_else(|| &no_args).to_owned(),
+            config,
+        )),
+        "vc_zkp_verify_proof" => runtime.block_on(vc_zkp_verify_proof(
+            arguments_vec.get(0).unwrap_or_else(|| &no_args).to_owned(),
+            options,
+            arguments_vec.get(1).unwrap_or_else(|| &no_args).to_owned(),
+            config,
+        )),
+        "run_custom_function" => runtime.block_on(run_custom_function(
+            arguments_vec.get(1).unwrap_or_else(|| &no_args).to_owned(),
+            arguments_vec.get(0).unwrap_or_else(|| &no_args).to_owned(),
+            options,
+            arguments_vec.get(2).unwrap_or_else(|| &no_args).to_owned(),
+            config,
+        )),
+
+        _ => Err("Function Not Supported By Vade".to_string()),
     };
-    
-    let response = match result
-     {
+
+    let response = match result.as_ref() {
         Ok(Some(value)) => value.to_string(),
         Ok(_) => "Unknown Result".to_string(),
         Err(e) => e.to_string(),
     };
-    return  CString::new(response).unwrap().into_raw();
+
+    println!(" respond {}", response);
+
+    return CString::new(response).unwrap().into_raw();
 }
