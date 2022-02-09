782603a7eece316fee3c0eb496f27b75b725514a
vineet pant <10172895+vineetpant@users.noreply.github.com>
2022-02-09 18:24:59 +0530

    changes to java wrapper class

--- builds/java/vade/src/main/java/com/vade/evan/Vade.java
+++ builds/java/vade/src/main/java/com/vade/evan/Vade.java
@@ -7,5 +7,9 @@ public final class Vade {
     private Vade() {
     }
 
+    static {
+        System.loadLibrary("vade_evan");
+    }
+
     public static native String ExecuteVade(String funcName, String[] arguments, String options, String config);
 }
