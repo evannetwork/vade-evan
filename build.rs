59f8ae04bdff74196f3c1945fdf43c1f9e6a6346
vineet pant <10172895+vineetpant@users.noreply.github.com>
2022-03-08 18:22:02 +0100

    fix pipeline

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
 // Custom build script.
 fn main() {
     // Tell Cargo to search particular directory for library dependency
-    println!("cargo:rustc-link-search=builds/c");
-    println!("cargo:rustc-link-lib=request_list");
+    // TODO: uncomment this line and point it to the directory which contains library
+    //println!("cargo:rustc-link-search=builds/c");
+    // TODO: uncomment this line and point it to actual request_lis to compile for sdk feature
+    // println!("cargo:rustc-link-lib=request_list");
 }
