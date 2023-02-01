51c6cc78070ffafbe0791df1371e4db2959311b4
vineet pant <10172895+vineetpant@users.noreply.github.com>
2023-02-01 18:10:29 +0530

    refactor test for credential request

--- src/helpers/credential.rs
+++ src/helpers/credential.rs
@@ -217,4 +217,38 @@ mod tests {
 
         Ok(())
     }
+
+    #[tokio::test]
+    async fn helper_can_create_credential_request() -> Result<()> {
+        let mut vade_evan = VadeEvan::new(crate::VadeEvanConfig {
+            target: "test",
+            signer: "remote|http://127.0.0.1:7070/key/sign",
+        })?;
+        let credential_offer = r#"{
+        "issuer": "did:evan:testcore:0x0d87204c3957d73b68ae28d0af961d3c72403906",
+        "subject": "did:any:abc",
+        "nonce": "QqJR4o6joiApYVXX7JLbRIZBQ9QprlFpewo8GbojIKY=",
+        "credentialMessageCount": 2
+    }"#;
+        let bbs_secret = r#""OASkVMA8q6b3qJuabvgaN9K1mKoqptCv4SCNvRmnWuI=""#;
+        let credential_values = r#"{
+        "email": "value@x.com"
+    }"#;
+        let issuer_pub_key = r#""jCv7l26izalfcsFe6j/IqtVlDolo2Y3lNld7xOG63GjSNHBVWrvZQe2O859q9JeVEV4yXtfYofGQSWrMVfgH5ySbuHpQj4fSgLu4xXyFgMidUO1sIe0NHRcXpOorP01o""#;
+
+        let credential_request = vade_evan
+            .helper_create_credential_request(
+                issuer_pub_key,
+                bbs_secret,
+                credential_values,
+                credential_offer,
+                SCHEMA_DID,
+            )
+            .await?;
+
+        println!("{}", credential_request);
+        assert!(credential_request.contains("blindSignatureContext"));
+
+        Ok(())
+    }
 }
